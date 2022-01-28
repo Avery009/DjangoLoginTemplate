16c2a484cb66ea965e465d6c5d24c34eef8b3a0f
James Avery King <jamesaveryking@Jamess-MacBook-Pro.local>
2022-01-27 19:41:59 -0500

    Adding database functionality 001

--- initialtemplate/initialtemplate/settings.py
+++ initialtemplate/initialtemplate/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'crispy_forms',
+    'login',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- initialtemplate/login/models.py
+++ initialtemplate/login/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+	userID = models.BigAutoField(primary_key=True),
+	username = models.CharField(blank=False,null=False,max_length=40)
+	password = models.CharField(blank=False,null=False,max_length=40)
